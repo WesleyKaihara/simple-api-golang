2363c3a0db8a794f9424718a8df198c448cc9449
Wesley Kaihara <wesleykaihara@gmail.com>
2024-08-12 21:05:07 -0300

    Initial connection and structure

---
+++ cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"api-golang/controller"
+	"api-golang/db"
+	"api-golang/repository"
+	"api-golang/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	dbConnection, err := db.ConnectDB()
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Repository Layer
+	ProductRepository := repository.NewProductRepository(dbConnection)
+
+	// UseCase Layer
+	ProductUseCase := usecase.NewProductUseCase(ProductRepository)
+
+	// Controllers Layer
+	ProductController := controller.NewProductController(ProductUseCase)
+
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.GET("/products", ProductController.GetProducts)
+
+	server.Run(":8000")
+}
---
+++ usecase/product_usecase.go
@@ -0,0 +1,20 @@
+package usecase
+
+import (
+	"api-golang/model"
+	"api-golang/repository"
+)
+
+type ProductUsecase struct {
+	repository repository.ProductRepository
+}
+
+func NewProductUseCase(repo repository.ProductRepository) ProductUsecase {
+	return ProductUsecase{
+		repository: repo,
+	}
+}
+
+func (pu *ProductUsecase) GetProducts() ([]model.Product, error) {
+	return pu.repository.GetProducts()
+}
